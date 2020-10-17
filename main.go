50e84a686233830f7a020f48301836710f8c7f12
Hassan Shahbazi <h-shahbazi@hotmail.com>
2020-10-17 16:32:22 +0300

    Part 2: Complex Int and String functions

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/wasmerio/go-ext-wasm/wasmer"
 )
@@ -35,19 +36,101 @@ func main() {
 	defer importObject.Close()
 	defer instance.Close()
 
-	start(&instance)
+	sum(&instance)
+	concatenate(&instance, "Hello", "World!")
 }
 
-func start(instance *wasmer.Instance) {
-	// Gets start function from the WebAssembly instance.
-	start := instance.Exports["_start"]
+func sum(instance *wasmer.Instance) {
+	// Gets sum function from the WebAssembly instance.
+	sum := instance.Exports["sum"]
 
 	// Calls that exported function with Go standard values. The WebAssembly types are inferred and values are casted automatically.
-	result, err := start()
+	result, err := sum(1, 2)
 	if err != nil {
 		panic(err)
 	}
 
-	// To ensure the start function doesn't return any values
-	fmt.Println(result.GetType() == wasmer.TypeVoid)
+	// To ensure the hello function doesn't return any values
+	fmt.Println(result.ToI32())
+}
+
+func concatenate(instance *wasmer.Instance, s1, s2 string) {
+	// Allocate memory function
+	allocate := instance.Exports["allocate"]
+
+	memoryPointerS1 := func() int32 {
+		// run allocate for s2
+		pointer, err := allocate(len(s1) + 1)
+		if err != nil {
+			panic(err)
+		}
+
+		// copy s2 to memory
+		memoryS1 := instance.Memory.Data()[pointer.ToI32():]
+		copy(memoryS1, s2)
+		memoryS1[len(s1)] = 0
+
+		return pointer.ToI32()
+	}()
+
+	memoryPointerS2 := func() int32 {
+		// run allocate for s2
+		pointer, err := allocate(len(s2) + 1)
+		if err != nil {
+			panic(err)
+		}
+
+		// copy s2 to memory
+		memoryS2 := instance.Memory.Data()[pointer.ToI32():]
+		copy(memoryS2, s2)
+		memoryS2[len(s2)] = 0
+
+		return pointer.ToI32()
+	}()
+
+	// Get concat function from the WebAssembly instance.
+	concat := instance.Exports["concatenate"]
+
+	// Calls that exported function with memory pointers.
+	result, err := concat(memoryPointerS1, memoryPointerS2)
+	if err != nil {
+		panic(err)
+	}
+
+	// The result is another memory pointer contains the concated strings
+	output, size := convertToString(instance, result)
+	fmt.Println(output)
+
+	// deallocate memory
+	deallocate := instance.Exports["deallocate"]
+
+	// deallocate memoryS1
+	_, err = deallocate(memoryPointerS1, len(s1))
+	if err != nil {
+		panic(err)
+	}
+
+	// deallocate memorySs
+	_, err = deallocate(memoryPointerS2, len(s2))
+	if err != nil {
+		panic(err)
+	}
+
+	// deallocate output memory
+	_, err = deallocate(result, size)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func convertToString(instance *wasmer.Instance, output wasmer.Value) (string, int32) {
+	memory := instance.Memory.Data()[output.ToI32():]
+	var builder strings.Builder
+	counter := 0
+
+	for memory[counter] != 0 {
+		builder.WriteByte(memory[counter])
+		counter++
+	}
+	return builder.String(), int32(counter)
 }
--- swiftwasm/Sources/swiftwasm/main.swift
+++ swiftwasm/Sources/swiftwasm/main.swift
@@ -1,1 +1,29 @@
 print("Hello, world!")
+
+@_cdecl("hello")
+func helloFromHost() {
+  print("The function is called from a host application")
+}
+
+@_cdecl("sum")
+func sumFromHost(x: Int32, y: Int32) -> Int32 {
+  return x + y
+}
+
+@_cdecl("allocate")
+func allocate(size: Int) -> UnsafeMutableRawPointer {
+  return UnsafeMutableRawPointer.allocate(byteCount: size, alignment: MemoryLayout<UInt8>.alignment)
+}
+@_cdecl("deallocate")
+func deallocate(pointer: UnsafeMutableRawPointer, size: Int) {
+  pointer.deallocate()
+}
+
+@_cdecl("concatenate")
+func concatenateFromHost(s1: UnsafePointer<CChar>, s2: UnsafePointer<CChar>) -> UnsafeMutablePointer<CChar> {
+  let str1 = String(cString: s1)
+  let str2 = String(cString: s2)
+  let result = str1 + " " + str2
+
+  return UnsafeMutablePointer<CChar>(mutating: result)
+}
