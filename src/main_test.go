336150ecedaedcdeadc56c543d21f191d796b8a8
Hassan Shahbazi <hassan@ninchat.com>
2020-11-30 16:32:54 +0200

    add benchmark and improve tests

--- src/main_test.go
+++ src/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"github.com/wasmerio/go-ext-wasm/wasmer"
@@ -9,28 +10,17 @@ import (
 var instance wasmer.Instance
 
 func TestMain(m *testing.M) {
-	_, _, instance = instantiate(swiftBinaryPath)
+	binary := os.Getenv("binary")
+
+	if binary == "swift" {
+		_, _, instance = instantiate(swiftBinaryPath)
+	} else if binary == "rust" {
+		_, _, instance = instantiate(rustBinaryPath)
+	} else {
+		panic("invalid binary type")
+	}
+
 	m.Run()
-}
-
-func TestStartBinary_Swift(t *testing.T) {
-	if start(&instance).GetType() != wasmer.TypeVoid {
-		t.Error("error in starting the project")
-	}
-}
-
-func TestExportedFunction_Swift(t *testing.T) {
-	result := sum(&instance)
-	if result != 3 {
-		t.Error("expected value is: 3, but got:", result)
-	}
-}
-
-func TestImportedFunction_Swift(t *testing.T) {
-	result := fetchCodeOnBinary(&instance, 2)
-	if result != 4 {
-		t.Error("expected value is: 4, but got:", result)
-	}
 }
 
 func TestStartBinary_Rust(t *testing.T) {
@@ -47,6 +37,26 @@ func TestExportedFunction_Rust(t *testing.T) {
 }
 
 func TestImportedFunction_Rust(t *testing.T) {
+	result := fetchCodeOnBinary(&instance, 2)
+	if result != 4 {
+		t.Error("expected value is: 4, but got:", result)
+	}
+}
+
+func TestStartBinary_Swift(t *testing.T) {
+	if start(&instance).GetType() != wasmer.TypeVoid {
+		t.Error("error in starting the project")
+	}
+}
+
+func TestExportedFunction_Swift(t *testing.T) {
+	result := sum(&instance)
+	if result != 3 {
+		t.Error("expected value is: 3, but got:", result)
+	}
+}
+
+func TestImportedFunction_Swift(t *testing.T) {
 	result := fetchCodeOnBinary(&instance, 2)
 	if result != 4 {
 		t.Error("expected value is: 4, but got:", result)
